f5ec26ddb457f9099fbf77dfb4f9cca8d8aeea77
QueenMaggie <wanjiumargaret394@gmail.com>
2024-02-06 08:29:31 -0800

    3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -24,7 +24,6 @@ def cisfun(text):
     """display “C ” followed by the value of the text variable"""
     return 'C ' + text.replace('_', ' ')
 
-
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def pythoniscool(text='is cool'):
